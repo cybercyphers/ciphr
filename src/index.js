b598a120029db9c33db74cc62b84879116444182
cyber <193218221+cybercyphers@users.noreply.github.com>
2026-08-14 22:54:05 +0000

    Add __Dirname function for module path resolution
    Defined __dirname as a function to be accessible in module type.

--- src/index.js
+++ src/index.js
@@ -25,8 +25,17 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 
 
+/*since in  type : module, __dirname  is not defined, we define it  here to be accessible without manually defining path.join(__dirname); to help developers. 
 
 
+
+the placeholder should always be "import.meta.url" nothing else in order to resolve to the user's path directly instead of cyphr path...
+*/
+
+
+const __Dirname(import_meta_url)=>{
+    return dirname(fileURLToPath(import_meta_url));
+}
 
  
 
@@ -43,6 +52,8 @@ const defaultTools = {
 }
 
 
-export default defaultTools;
+export default defaultTools, {
+    __Dirname
+};
 
 //EOF
