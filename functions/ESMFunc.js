c765a50999fbe35fe4f3eee068211bb81d4e4b0a
cyber <193218221+cybercyphers@users.noreply.github.com>
2026-08-16 13:14:59 +0000

    Update ESMFunc.js

--- functions/ESMFunc.js
+++ functions/ESMFunc.js
@@ -1,3 +1,19 @@
+
+/*
+ _dirname also refered as __dirname but since javascript is case-sensitive, we can use it again.
+*/
+
+
+/*
+ this expression is made to prevent the errors caused when build with tsup or esbuild since in commonJS, import.meta.url is not available, this finc will switch between ESM AND CJS smoothly..
+*/
+
+
+
+
+
+
+
 const _dirname =typeof __dirname !== 'undefined' ? __dirname : dirname(fileURLToPath(import.meta.url));
 
 
