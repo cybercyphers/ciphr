e2475217127dc1ab4963058e8d79e22f8d49a2c3
cyber <193218221+cybercyphers@users.noreply.github.com>
2026-08-16 22:58:48 +0000

    Remove cache directory option from execsync command
    Removed the '--rm-cache-dir' option to prevent cache clearing.

--- functions/systemFuncs.js
+++ functions/systemFuncs.js
@@ -156,8 +156,6 @@ return { video_only_hp:`${data[0]}\n`,
    
   const { stdout,stderr } = await execsync(path.join(_dirname,"../binaries/ytdlp"),[
        "--no-cookies",
-  // Erase the local execution cache to prevent sticky 403 CDN blocks
-  "--rm-cache-dir",
       "--user-agent",
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
  "-f",
