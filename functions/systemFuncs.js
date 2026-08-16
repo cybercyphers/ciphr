ce20402de0a91eec94dded55978d60589d70334e
cyber <193218221+cybercyphers@users.noreply.github.com>
2026-08-16 23:01:25 +0000

    Update systemFuncs.js

--- functions/systemFuncs.js
+++ functions/systemFuncs.js
@@ -155,7 +155,7 @@ return { video_only_hp:`${data[0]}\n`,
        
    
   const { stdout,stderr } = await execsync(path.join(_dirname,"../binaries/ytdlp"),[
-       "--no-cookies",
+
       "--user-agent",
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
  "-f",
